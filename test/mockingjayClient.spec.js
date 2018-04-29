5d8b5ec3139ea31365f0705093927c4e09d3c480
Liam Griffin <gryffskin@gmail.com>
2018-04-29 11:33:39 +0100

    Test mockingjayClient with data

--- test/mockingjayClient.spec.js
+++ test/mockingjayClient.spec.js
@@ -3,15 +3,40 @@ import nock from 'nock'
 
 import mockingjayClient from '../src/mockingjayClient'
 
+const mockingjayUrl = 'http://localhost:9099'
+
 test('makes a call to mockingjay server to get requests', async t => {
-  const mockingjayUrl = 'http://localhost:9099'
   const mjClient = mockingjayClient(mockingjayUrl)
 
-  nock(mockingjayUrl)
-    .get('/requests')
-    .reply(200, [])
+  mockMockingjay([])
 
   const mjRequests = await mjClient.getRequests()
 
   t.deepEqual(mjRequests, [])
 })
+
+test('returns data from mockingjay without transforming', async t => {
+  const mjClient = mockingjayClient(mockingjayUrl)
+
+  const data = ['/one', '/two', '/three'].map(mockingjayGETRequestBuilder)
+  mockMockingjay(data)
+
+  const mjRequests = await mjClient.getRequests()
+
+  t.deepEqual(mjRequests, data)
+})
+
+function mockMockingjay (data) {
+  nock(mockingjayUrl)
+    .get('/requests')
+    .reply(200, data)
+}
+
+function mockingjayGETRequestBuilder(uri) {
+  return {
+    URI: uri,
+    Method: 'GET',
+    Body: '',
+    Form: null
+  }
+}
