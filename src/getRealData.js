144287af09e67ffd746d8984c58c518ca08e71fa
Liam Griffin <gryffskin@gmail.com>
2018-05-03 14:41:07 +0100

    Make error messages more helpful when http requests fail

--- src/getRealData.js
+++ src/getRealData.js
@@ -6,4 +6,8 @@ function getRealData (baseUrl, uris) {
   return Promise
     .all(uris.map(uri => request(`${baseUrl}${uri}`)))
     .then(datas => datas.map(JSON.parse))
+    .catch(err => {
+      err.message = `Error getting real data:\n${err.message}`
+      throw err
+    })
 }
